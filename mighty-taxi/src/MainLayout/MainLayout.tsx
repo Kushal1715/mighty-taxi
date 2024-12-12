decc1360da41a91d7c0552543acb58733ac85f97
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-12 18:20:36 +0545

    adding navbar from mui

--- mighty-taxi/src/MainLayout/MainLayout.tsx
+++ mighty-taxi/src/MainLayout/MainLayout.tsx
@@ -1,4 +1,6 @@
 import SideNav from '@/components/sidenav'
+import Navbar from '@/components/sidenav/Navbar'
+import { ClassNames } from '@emotion/react'
 import { Box } from '@mui/material'
 import { Outlet } from 'react-router-dom'
 
@@ -8,8 +10,11 @@ const MainLayout = (props: Props) => {
   return (
     <><Box sx={{ display: 'flex' }}>
       <SideNav />
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Outlet />
+      <Box component="main" sx={{ flexGrow: 1, p: 0 }}>
+        <Navbar />
+        <Box component="main" sx={{ flexGrow: 1, backgroundColor: '#f7f9ff', }}>
+          <Outlet />
+        </Box>
       </Box>
     </Box></>
   )
