7bcb884be57e742477e685b0da4a2976e4268b97
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-16 13:01:16 +0545

    login form

--- mighty-taxi/src/pages/Login/LoginPage.tsx
+++ mighty-taxi/src/pages/Login/LoginPage.tsx
@@ -1,10 +1,47 @@
-import React from 'react'
 
 type Props = {}
 
 const LoginPage = (props: Props) => {
   return (
-    <div>LoginPage</div>
+    <>
+      <section className="bg-[#222]">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <img className="w-16 h-16" src="/logo.png" alt="logo" />
+          </a>
+          <div className="w-full bg-black text-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight  md:text-2xl text-white">
+                Sign in to your account
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
+                  <input type="email" name="email" id="email" className="bg-[#222] border border-gray-300  rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                </div>
+                <div>
+                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Password</label>
+                  <input type="password" name="password" id="password" placeholder="••••••••" className="bg-[#222] border border-gray-300  rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label htmlFor="remember" className="text-white dark:text-gray-300">Remember me</label>
+                    </div>
+                  </div>
+                  <a href="#" className="text-sm font-medium text-blue-500 hover:underline ">Forgot password?</a>
+                </div>
+                <button type="submit" className="w-full text-white bg-blue-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+
   )
 }
 
