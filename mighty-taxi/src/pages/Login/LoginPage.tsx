f5ec09f2e16b619f9a0b0de8759d63f5a5fdf827
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-16 13:26:08 +0545

    formik and yup for handling login form

--- mighty-taxi/src/pages/Login/LoginPage.tsx
+++ mighty-taxi/src/pages/Login/LoginPage.tsx
@@ -1,7 +1,23 @@
-
+import * as Yup from 'yup'
+import { useFormik } from 'formik'
 type Props = {}
 
 const LoginPage = (props: Props) => {
+  const validationSchema = Yup.object({
+    email: Yup.string().email('Invalid email address').required('email address is required'),
+    password: Yup.string().min(5, 'password must be at least 5 characters long').required('password is required')
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
   return (
     <>
       <section className="bg-[#222]">
@@ -14,19 +30,27 @@ const LoginPage = (props: Props) => {
               <h1 className="text-xl font-bold leading-tight tracking-tight  md:text-2xl text-white">
                 Sign in to your account
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
+              <form className="space-y-4 md:space-y-6" onSubmit={formik.handleSubmit}>
                 <div>
                   <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
-                  <input type="email" name="email" id="email" className="bg-[#222] border border-gray-300  rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                  <input type="email" name="email" id="email" className="bg-[#222] border border-gray-300  rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.email} />
+                  {formik.touched.email && formik.errors.email && <p className='text-red-500'>{formik.errors.email}</p>}
                 </div>
                 <div>
                   <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Password</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••" className="bg-[#222] border border-gray-300  rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                  <input type="password" name="password" id="password" placeholder="••••••••" className="bg-[#222] border border-gray-300  rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password} />
+                  {formik.touched.password && formik.errors.password && <p className='text-red-500'>{formik.errors.password}</p>}
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
                     <div className="flex items-center h-5">
-                      <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
+                      <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" />
                     </div>
                     <div className="ml-3 text-sm">
                       <label htmlFor="remember" className="text-white dark:text-gray-300">Remember me</label>
